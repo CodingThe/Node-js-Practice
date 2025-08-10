1304b78d51c2567032fdd490bda4794d9759c0b7
CodingThe <prakashthakur07062002@gmail.com>
2025-08-10 11:45:02 +0530

    error handling

--- expressServer.js
+++ expressServer.js
@@ -2,17 +2,53 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// middleware
+// global middleware
 app.use((req,res,next)=>{
     console.log("method",req.method);
     console.log("url",req.url);
     next();
 })
 
+// route level
+const chechAuth = (req,res,next)=>{
+    console.log("this is route level middleware");
+    next();
+}
+
+// inbilt
+app.use(express.json());
+
+//error handling
+app.use((err,req,res,next)=>{
+    console.log("this is error",err.message);
+    res.status(500).send("something broke");
+}) 
+
+const users =[{id:1, Name: "john", greet:"hi"}];
+
 // routes
 app.get('/',(req,res)=>{
     res.send("welcome to home page");
 })
+
+app.get('/getUsers', chechAuth, (req ,res) => {
+    res.send(users);
+})
+
+
+app.get('/getUsersById/:id', (req ,res) => {
+    res.send(users.filter(user=>user.id == req.params.id));
+})
+
+app.post('/registerUser',(req,res)=>{
+    users.push(req.body.user);
+    res.send(users);
+})
+
+app.post('/updateUser',(req,res)=>{
+    users[req.body.user.id] =req.body.user;
+})
+
 
 app.listen(PORT,()=>{
     console.log("app started");
